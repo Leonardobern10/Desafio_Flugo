929591a5a3c42f96fae874a495c0d245dc68692f
Leonardo Bernardo <leonardo.bernardo2658@gmail.com>
2025-08-07 14:42:31 -0300

    update: importação de dotenv

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -2,6 +2,7 @@ import "@fontsource/roboto/300.css";
 import "@fontsource/roboto/400.css";
 import "@fontsource/roboto/500.css";
 import "@fontsource/roboto/700.css";
+import "dotenv/config";
 
 import "./App.css";
 import ButtonUsage from "../components/Buttons/ButtonUsage";
