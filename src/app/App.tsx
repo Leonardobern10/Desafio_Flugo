188ada35be2aa683205ce92793f01413d2e1b44f
Leonardo Bernardo <leonardo.bernardo2658@gmail.com>
2025-08-07 18:17:48 -0300

    fix: corrigindo importação de componentes

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,18 +1,37 @@
-import "@fontsource/roboto/300.css";
-import "@fontsource/roboto/400.css";
-import "@fontsource/roboto/500.css";
-import "@fontsource/roboto/700.css";
-import "dotenv/config";
-
-import "./App.css";
-import ButtonUsage from "../components/Buttons/ButtonUsage";
+import {Box, Grid} from '@mui/material';
+import SideBar from '../components/SideBar';
+import ContainerAvatar from '../components/ContainerAvatar';
+import MainContent from '../components/MainContent';
 
 function App() {
-  return (
-    <div>
-      <ButtonUsage />
-    </div>
-  );
+     return (
+          <Box
+               sx={{
+                    p: 2,
+                    bgcolor: '#FFF',
+                    width: '100dvw',
+                    height: '100dvh',
+                    display: 'flex',
+                    flexDirection: 'row',
+                    justifyContent: 'space-between',
+                    alignItems: 'center',
+               }}>
+               <SideBar />
+               <Grid
+                    sx={{
+                         height: '100%',
+                         width: '100%',
+                         display: 'flex',
+                         flexDirection: 'column',
+                         justifyContent: 'center',
+                         alignItems: 'end',
+                         rowGap: 5,
+                    }}>
+                    <ContainerAvatar />
+                    <MainContent />
+               </Grid>
+          </Box>
+     );
 }
 
 export default App;
