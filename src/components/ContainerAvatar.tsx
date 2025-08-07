5b152ef87456d9d9839010bec926e095d8f8d5e1
Leonardo Bernardo <leonardo.bernardo2658@gmail.com>
2025-08-07 18:18:04 -0300

    add: adição de comentário

--- src/components/ContainerAvatar.tsx
+++ src/components/ContainerAvatar.tsx
@@ -2,6 +2,10 @@ import {Box} from '@mui/material';
 import type {ReactElement} from 'react';
 import UserAvatar from './Header/UserAvatar';
 
+/**
+ * Talvez trocar esse box por um icone
+ */
+
 export default function ContainerAvatar(): ReactElement {
      return (
           <Box
