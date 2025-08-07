02404f7a189e2ca7b378cbb12d9dbbeeef3bf2e4
Leonardo Bernardo <leonardo.bernardo2658@gmail.com>
2025-08-07 11:46:36 -0300

    fix: alterando localização nas pastas

---
+++ src/app/App.tsx
@@ -0,0 +1,17 @@
+import "@fontsource/roboto/300.css";
+import "@fontsource/roboto/400.css";
+import "@fontsource/roboto/500.css";
+import "@fontsource/roboto/700.css";
+
+import "./App.css";
+import ButtonUsage from "../components/Buttons/ButtonUsage";
+
+function App() {
+  return (
+    <div>
+      <ButtonUsage />
+    </div>
+  );
+}
+
+export default App;
