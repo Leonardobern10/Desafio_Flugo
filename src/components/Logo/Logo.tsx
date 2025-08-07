63a38c7271d38c323e3310d700b807853b5071f1
Leonardo Bernardo <leonardo.bernardo2658@gmail.com>
2025-08-07 18:19:46 -0300

    new: criação do componente que recebe a logo

--- src/components/Logo/Logo.tsx
+++ src/components/Logo/Logo.tsx
@@ -1,10 +1,11 @@
 import type {ReactElement} from 'react';
 import logo from '../../assets/flugo_logo.svg';
+import {Box} from '@mui/material';
 
 export default function Logo(): ReactElement {
      return (
-          <div>
+          <Box>
                <img src={logo} alt="Logo da empresa" />
-          </div>
+          </Box>
      );
 }
