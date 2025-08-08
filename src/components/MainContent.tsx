1b212ba6f4ab446178144efea38114ffb1cadd55
Leonardo Bernardo <leonardo.bernardo2658@gmail.com>
2025-08-08 14:30:06 -0300

    fix: ajustando estlização

--- src/components/MainContent.tsx
+++ src/components/MainContent.tsx
@@ -1,12 +1,44 @@
-import {Box} from '@mui/material';
+import {Box, Button, Typography} from '@mui/material';
 import type {ReactElement} from 'react';
+import FormDefault from '../context/TableColaboradores';
 
 export default function MainContent(): ReactElement {
      return (
           <Box
                component={'main'}
-               sx={{width: '100%', height: '100%', border: 2}}>
-               COnteudo principal
+               sx={{
+                    width: '100%',
+                    height: '100%',
+                    display: 'flex',
+                    flexDirection: 'column',
+                    justifyContent: 'space-between',
+                    alignItems: 'stretch',
+               }}>
+               <Box
+                    sx={{
+                         display: 'flex',
+                         flexDirection: 'row',
+                         justifyContent: 'space-between',
+                         alignItems: 'center',
+                         height: 'fit-content',
+                    }}>
+                    <Typography variant="h5" sx={{color: 'text.primary'}}>
+                         Colaboradores
+                    </Typography>
+                    <Button
+                         sx={{color: '#fff', borderRadius: 2, paddingY: 1}}
+                         variant="contained"
+                         size="large"
+                         color="primary">
+                         Novo Colaborador
+                    </Button>
+               </Box>
+               <Box
+                    sx={{
+                         height: '80%',
+                    }}>
+                    <FormDefault />
+               </Box>
           </Box>
      );
 }
