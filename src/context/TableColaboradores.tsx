9d1d4e4bad8a0150a14bf4099af210988a1b0ae0
Leonardo Bernardo <leonardo.bernardo2658@gmail.com>
2025-08-08 14:31:01 -0300

    new: definição da estrutura para tabela de funcionários

---
+++ src/context/TableColaboradores.tsx
@@ -0,0 +1,44 @@
+import {
+     Paper,
+     Table,
+     TableBody,
+     TableCell,
+     TableContainer,
+     TableHead,
+     TableRow,
+} from '@mui/material';
+import type {ReactElement} from 'react';
+import {headerTableDatas} from '../data/tableData';
+
+export default function FormDefault(): ReactElement {
+     return (
+          <TableContainer component={Paper}>
+               <Table>
+                    <TableHead>
+                         <TableRow>
+                              {headerTableDatas.map((el) => (
+                                   <TableCell
+                                        sx={{
+                                             color: 'text.secondary',
+                                             fontWeight: 600,
+                                             fontSize: 14,
+                                             lineHeight: '24px',
+                                        }}
+                                        key={el.index}>
+                                        {el.title}
+                                   </TableCell>
+                              ))}
+                         </TableRow>
+                    </TableHead>
+                    <TableBody>
+                         <TableRow>
+                              <TableCell>Fernanda Torres</TableCell>
+                              <TableCell>fernandaTorres@flugo.com</TableCell>
+                              <TableCell>Design</TableCell>
+                              <TableCell>inativo</TableCell>
+                         </TableRow>
+                    </TableBody>
+               </Table>
+          </TableContainer>
+     );
+}
