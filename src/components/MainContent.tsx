a96568de44f151967274b786c4075f5c9b3a1a98
Leonardo Bernardo <leonardo.bernardo2658@gmail.com>
2025-08-08 18:41:08 -0300

    new: região que receberá o formulario

--- src/components/MainContent.tsx
+++ src/components/MainContent.tsx
@@ -1,8 +1,11 @@
 import {Box, Button, Typography} from '@mui/material';
-import type {ReactElement} from 'react';
-import FormDefault from '../context/TableColaboradores';
+import {useState, type ReactElement} from 'react';
+import TableEmployee from '../context/TableColaboradores';
+import FormContext from '../context/FormContext';
 
 export default function MainContent(): ReactElement {
+     const [showStepper, setShowStepper] = useState(false);
+
      return (
           <Box
                component={'main'}
@@ -14,31 +17,44 @@ export default function MainContent(): ReactElement {
                     justifyContent: 'space-between',
                     alignItems: 'stretch',
                }}>
-               <Box
-                    sx={{
-                         display: 'flex',
-                         flexDirection: 'row',
-                         justifyContent: 'space-between',
-                         alignItems: 'center',
-                         height: 'fit-content',
-                    }}>
-                    <Typography variant="h5" sx={{color: 'text.primary'}}>
-                         Colaboradores
-                    </Typography>
-                    <Button
-                         sx={{color: '#fff', borderRadius: 2, paddingY: 1}}
-                         variant="contained"
-                         size="large"
-                         color="primary">
-                         Novo Colaborador
-                    </Button>
-               </Box>
-               <Box
-                    sx={{
-                         height: '80%',
-                    }}>
-                    <FormDefault />
-               </Box>
+               {showStepper ? (
+                    <FormContext />
+               ) : (
+                    <Box>
+                         <Box
+                              sx={{
+                                   display: 'flex',
+                                   flexDirection: 'row',
+                                   justifyContent: 'space-between',
+                                   alignItems: 'center',
+                                   height: 'fit-content',
+                              }}>
+                              <Typography
+                                   variant="h5"
+                                   sx={{color: 'text.primary'}}>
+                                   Colaboradores
+                              </Typography>
+                              <Button
+                                   sx={{
+                                        color: '#fff',
+                                        borderRadius: 2,
+                                        paddingY: 1,
+                                   }}
+                                   onClick={() => setShowStepper(true)}
+                                   variant="contained"
+                                   size="large"
+                                   color="primary">
+                                   Novo Colaborador
+                              </Button>
+                         </Box>
+                         <Box
+                              sx={{
+                                   height: '80%',
+                              }}>
+                              <TableEmployee />
+                         </Box>
+                    </Box>
+               )}
           </Box>
      );
 }
