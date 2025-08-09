5c1e3b9a83f0dc43bd804a52df1b9fcda2304b5e
Leonardo Bernardo <leonardo.bernardo2658@gmail.com>
2025-08-09 14:28:16 -0300

    fix: correção e separação de responsabilidades

--- src/components/Step2Form.tsx
+++ src/components/Step2Form.tsx
@@ -1,42 +1,35 @@
-import {Box, TextField} from '@mui/material';
+import {Box, MenuItem, TextField} from '@mui/material';
 import type {ReactElement} from 'react';
-import {useForm} from 'react-hook-form';
-import {useEmployeeFormStore} from '../store/employerFormStore';
+import {useFormContext} from 'react-hook-form';
+import {allDepartaments} from '../data/allDepartaments';
 
-export default function Step2Form(props: {
-     onSubmit: (data: any) => void;
-}): ReactElement {
-     const {formData, setFormData} = useEmployeeFormStore();
-
+export default function Step2Form(): ReactElement {
      const {
           register,
-          handleSubmit,
           formState: {errors},
-     } = useForm({
-          defaultValues: formData,
-     });
+     } = useFormContext();
 
-     const submit = (data: any) => {
-          console.log(formData);
-          setFormData(data);
-          console.log(data);
-          console.log(formData);
-          props.onSubmit;
-     };
+     // Exemplo de opções para o sele
 
      return (
-          <form onSubmit={handleSubmit(submit)}>
-               <Box sx={{display: 'flex', flexDirection: 'column', gap: 2}}>
-                    <TextField
-                         label="Departamento"
-                         {...register('departamento', {
-                              required: 'Departamento é obrigatório',
-                         })}
-                         error={!!errors.departamento}
-                         helperText={errors.departamento?.message}
-                    />
-                    <button type="submit" style={{display: 'none'}}></button>
-               </Box>
-          </form>
+          <Box sx={{display: 'flex', flexDirection: 'column', gap: 2}}>
+               <TextField
+                    select
+                    label="Selecione um departamento"
+                    defaultValue=""
+                    {...register('departamento', {
+                         required: 'Departamento é obrigatório',
+                    })}
+                    error={!!errors.departamento}
+                    helperText={errors.departamento?.message?.toString()}>
+                    {allDepartaments.map((option) => (
+                         <MenuItem
+                              key={option.index}
+                              value={option.departament}>
+                              {option.departament}
+                         </MenuItem>
+                    ))}
+               </TextField>
+          </Box>
      );
 }
