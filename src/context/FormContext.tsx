ecaccd7ca31cd321561232270f3b376bd62335da
Leonardo Bernardo <leonardo.bernardo2658@gmail.com>
2025-08-09 14:30:06 -0300

    fix: correção de importação dos componentes

--- src/context/FormContext.tsx
+++ src/context/FormContext.tsx
@@ -1,51 +1,65 @@
 import {useState, type ReactElement} from 'react';
-import {
-     Box,
-     Breadcrumbs,
-     Button,
-     Container,
-     LinearProgress,
-     Link,
-     Step,
-     StepLabel,
-     Stepper,
-     Typography,
-} from '@mui/material';
+import {Box, Container, Typography} from '@mui/material';
 import Step1Form from '../components/Step1Form';
 import Step2Form from '../components/Step2Form';
-import {useEmployeeFormStore} from '../store/employerFormStore';
+import {FormProvider, useForm} from 'react-hook-form';
+import type {EmployerFormDataType} from '../types/EmployerFormDataType';
+import ProgressForm from '../components/ProgressForm';
+import GuideSteps from '../components/GuideSteps';
+import ContainerButtons from '../components/ContainerButtons';
+import HeaderForm from '@components/HeaderForm';
+import {createUser, getEmployees} from '../services/employeeService';
 
 export default function FormContext(): ReactElement {
      // Gerencia o progresso no processo de cadastro
      const [progress, setProgress] = useState<number>(0);
-
+     // Gerencia o titulo a ser exibido no step
+     const [titleStep, setTitleStep] = useState<number>(0);
      // Gerencia a etapa atual no cadastro
      const [activeStep, setActiveStep] = useState<number>(0);
 
-     // Acesso das informações definidas na store
-     const {formData, setFormData, resetForm} = useEmployeeFormStore();
+     const formData = useForm<EmployerFormDataType>({
+          defaultValues: {
+               titulo: '',
+               email: '',
+               departamento: '',
+               status: '',
+          },
+     });
 
      // Funções para transitar entre os passos
-     const handleNext = () => {
-          setActiveStep((prev) => prev + 1);
-          setProgress(progress + 50);
+     const handleNext = async () => {
+          const isValid = await formData.trigger();
+          if (isValid) {
+               setActiveStep((prev) => prev + 1);
+               setProgress(progress + 50);
+               setTitleStep((prev) => prev + 1);
+          }
      };
+
      const handleBack = () => {
           setActiveStep((prev) => prev - 1);
           setProgress(progress - 50);
+          setTitleStep((prev) => prev - 1);
      };
 
-     const handleFinish = () => {
-          console.log({
-               titulo: formData.titulo,
-               email: formData.email,
-               departamento: formData.departamento,
-               status: formData.status,
-          });
-          alert('Cadastro concluído!\n' + JSON.stringify(formData, null, 2));
-          resetForm();
+     const handleFinish = (data: any) => {
+          const dataToSave = {
+               ...data,
+               status: data.status ? 'ativo' : 'inativo',
+          };
+          alert('Cadastro concluído!\n' + JSON.stringify(dataToSave, null, 2));
+          reset();
+          createUser(dataToSave);
+          getEmployees();
+     };
+
+     // Método que reseta todas as variaveis
+     const reset = () => {
           setActiveStep(0);
           setProgress(0);
+          setTitleStep(0);
+          formData.reset();
      };
 
      const steps = ['Infos Básicas', 'Infos Profissionais'];
@@ -53,9 +67,9 @@ export default function FormContext(): ReactElement {
      const renderStep = () => {
           switch (activeStep) {
                case 0:
-                    return <Step1Form onSubmit={handleNext} />;
+                    return <Step1Form />;
                case 1:
-                    return <Step2Form onSubmit={handleFinish} />;
+                    return <Step2Form />;
                default:
                     return null;
           }
@@ -63,77 +77,46 @@ export default function FormContext(): ReactElement {
 
      return (
           <Box>
-               <Breadcrumbs separator=">" aria-label="breadcrumb">
-                    <Link underline="none">
-                         <Typography color="text.primary">
-                              Colaboradores
-                         </Typography>
-                    </Link>
-                    <Link underline="none">
-                         <Typography color="text.secondary">
-                              Cadastrar coladoradores
-                         </Typography>
-                    </Link>
-               </Breadcrumbs>
-               <Box
-                    sx={{
-                         width: '100%',
-                         display: 'flex',
-                         flexDirection: 'row',
-                         justifyContent: 'space-between',
-                         alignItems: 'center',
-                    }}>
-                    <LinearProgress
-                         sx={{width: '95%'}}
-                         variant="determinate"
-                         value={progress}
-                    />
-                    <Typography>{progress}%</Typography>
-               </Box>
-               <Container maxWidth="sm" sx={{mt: 4}}>
-                    <Stepper activeStep={activeStep} alternativeLabel>
-                         {steps.map((label) => (
-                              <Step key={label}>
-                                   <StepLabel>{label}</StepLabel>
-                              </Step>
-                         ))}
-                    </Stepper>
-
-                    <Box sx={{mt: 4}}>{renderStep()}</Box>
-
+               <HeaderForm />
+               <ProgressForm progress={progress} />
+               <Container maxWidth="xl" sx={{mt: 4}}>
                     <Box
                          sx={{
                               display: 'flex',
-                              justifyContent: 'space-between',
-                              mt: 4,
+                              gap: 4,
+                              minHeight: '280px',
                          }}>
-                         <Button
-                              disabled={activeStep === 0}
-                              onClick={handleBack}
-                              variant="outlined">
-                              Voltar
-                         </Button>
-                         {activeStep < steps.length - 1 && (
-                              <Button
-                                   onClick={() =>
-                                        document
-                                             .querySelector('form')!
-                                             .requestSubmit()
-                                   }
-                                   variant="contained">
-                                   Próximo
-                              </Button>
-                         )}
-                         {activeStep === steps.length - 1 && (
-                              <Button
-                                   onClick={handleFinish}
-                                   variant="contained"
-                                   color="primary">
-                                   Finalizar
-                              </Button>
-                         )}
+                         {/* Stepper */}
+                         <GuideSteps activeStep={activeStep} steps={steps} />
+                         {/* Formulário */}
+                         <FormProvider {...formData}>
+                              <Box
+                                   sx={{
+                                        display: 'flex',
+                                        flexDirection: 'column',
+                                        justifyContent: 'center',
+                                        alignItems: 'stretch',
+                                        rowGap: 4,
+                                        width: '100%',
+                                   }}>
+                                   <Typography
+                                        variant="h5"
+                                        color="text.secondary">
+                                        {steps[titleStep]}
+                                   </Typography>
+                                   <Box sx={{flex: 1}}>{renderStep()}</Box>
+                              </Box>
+                         </FormProvider>
                     </Box>
+                    <ContainerButtons
+                         activeStep={activeStep}
+                         steps={steps}
+                         onBack={handleBack}
+                         onNext={handleNext}
+                         onSubmit={formData.handleSubmit(handleFinish)}
+                    />
                </Container>
+               <button onClick={getEmployees}>Search</button>
           </Box>
      );
 }
