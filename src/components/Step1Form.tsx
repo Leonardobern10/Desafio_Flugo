59af07678aa8645e51230a476b1a03cb83f7c8d4
Leonardo Bernardo <leonardo.bernardo2658@gmail.com>
2025-08-09 14:27:55 -0300

    fix: correção e separação de responsabilidades

--- src/components/Step1Form.tsx
+++ src/components/Step1Form.tsx
@@ -1,66 +1,49 @@
-import {useState, type ReactElement} from 'react';
-import {Controller, useForm} from 'react-hook-form';
-
+import {type ReactElement} from 'react';
+import {Controller, useFormContext} from 'react-hook-form';
 import {Box, TextField} from '@mui/material';
 import ControlledSwitch from './ControlledSwitch';
-import {useEmployeeFormStore} from '../store/employerFormStore';
 
-export default function Step1Form(props: {onSubmit: Function}): ReactElement {
-     const {formData, setFormData} = useEmployeeFormStore();
-
+export default function Step1Form(): ReactElement {
      const {
           register,
-          handleSubmit,
           control,
           formState: {errors},
-     } = useForm({
-          defaultValues: formData,
-     });
+     } = useFormContext();
 
-     const submit = (data: any) => {
-          console.log(data);
-          setFormData(data);
-          props.onSubmit();
-     };
      return (
-          <form onSubmit={handleSubmit(submit)}>
-               <Box sx={{display: 'flex', flexDirection: 'column', gap: 2}}>
-                    <TextField
-                         label="Titulo"
-                         {...register('titulo', {
-                              required: 'Nome é obrigatório',
-                         })}
-                         error={!!errors.titulo}
-                         helperText={errors.titulo?.message}
-                    />
+          <Box sx={{display: 'flex', flexDirection: 'column', gap: 2}}>
+               <TextField
+                    label="Titulo"
+                    {...register('titulo', {
+                         required: 'Nome é obrigatório',
+                    })}
+                    error={!!errors.titulo}
+                    helperText={errors.titulo?.message?.toString()}
+               />
 
-                    <TextField
-                         label="Email"
-                         {...register('email', {
-                              required: 'Email é obrigatório',
-                              pattern: {
-                                   value: /\S+@\S+\.\S+/,
-                                   message: 'Email inválido',
-                              },
-                         })}
-                         error={!!errors.email}
-                         helperText={errors.email?.message}
-                    />
+               <TextField
+                    label="Email"
+                    {...register('email', {
+                         required: 'Email é obrigatório',
+                         pattern: {
+                              value: /\S+@\S+\.\S+/,
+                              message: 'Email inválido',
+                         },
+                    })}
+                    error={!!errors.email}
+                    helperText={errors.email?.message?.toString()}
+               />
 
-                    <Controller
-                         name="status"
-                         control={control}
-                         defaultValue={'true'}
-                         render={({field}) => (
-                              <ControlledSwitch
-                                   checked={field.value}
-                                   onChange={field.onChange}
-                              />
-                         )}
-                    />
-
-                    <button type="submit" style={{display: 'none'}}></button>
-               </Box>
-          </form>
+               <Controller
+                    name="status"
+                    control={control}
+                    render={({field}) => (
+                         <ControlledSwitch
+                              checked={field.value}
+                              onChange={(_, checked) => field.onChange(checked)}
+                         />
+                    )}
+               />
+          </Box>
      );
 }
