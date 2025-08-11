c09f2d4c37b5b1c5533cfb2fc369b58be631e894
Leonardo Bernardo <leonardo.bernardo2658@gmail.com>
2025-08-10 22:37:18 -0300

    feat: implementando consulta ao banco firestone para retornar os dados

--- src/context/TableColaboradores.tsx
+++ src/context/TableColaboradores.tsx
@@ -1,18 +1,59 @@
 import {
-     Paper,
+     Box,
+     CircularProgress,
+     Stack,
      Table,
      TableBody,
      TableCell,
      TableContainer,
      TableHead,
      TableRow,
+     Typography,
 } from '@mui/material';
-import type {ReactElement} from 'react';
+import {useEffect, useState, type ReactElement} from 'react';
 import {headerTableDatas} from '../data/tableData';
+import type {EmployerFormDataType} from 'types/EmployerFormDataType';
+import {getEmployees} from '@services/employeeService';
+import ArrowDownwardIcon from '@mui/icons-material/ArrowDownward';
+import EmployeeRow from '@components/Table/EmployeeRow';
 
 export default function TableEmployee(): ReactElement {
+     const [employees, setEmployees] = useState<EmployerFormDataType[]>([]);
+     const [loading, setLoading] = useState(true);
+
+     useEffect(() => {
+          const fetchEmployees = async () => {
+               try {
+                    const data = await getEmployees();
+                    setEmployees(data);
+               } finally {
+                    setLoading(false);
+               }
+          };
+          fetchEmployees();
+     }, []);
+
+     if (loading) {
+          return (
+               <Box
+                    sx={{
+                         display: 'flex',
+                         justifyContent: 'center',
+                         alignItems: 'center',
+                         height: '200px', // altura para centralizar spinner
+                    }}>
+                    <CircularProgress />
+               </Box>
+          );
+     }
+
      return (
-          <TableContainer component={Paper}>
+          <TableContainer
+               sx={{
+                    maxHeight: '70dvh',
+                    overflowY: 'auto',
+               }}
+               component={Box}>
                <Table>
                     <TableHead>
                          <TableRow>
@@ -25,18 +66,29 @@ export default function TableEmployee(): ReactElement {
                                              lineHeight: '24px',
                                         }}
                                         key={el.index}>
-                                        {el.title}
+                                        <Stack
+                                             sx={{
+                                                  display: 'flex',
+                                                  flexDirection: 'row',
+                                                  justifyContent: 'flex-start',
+                                                  alignItems: 'center',
+                                                  columnGap: 1,
+                                             }}>
+                                             <Typography
+                                                  fontWeight={500}
+                                                  fontSize="small">
+                                                  {el.title}
+                                             </Typography>
+                                             <ArrowDownwardIcon fontSize="inherit" />
+                                        </Stack>
                                    </TableCell>
                               ))}
                          </TableRow>
                     </TableHead>
                     <TableBody>
-                         <TableRow>
-                              <TableCell>Fernanda Torres</TableCell>
-                              <TableCell>fernandaTorres@flugo.com</TableCell>
-                              <TableCell>Design</TableCell>
-                              <TableCell>inativo</TableCell>
-                         </TableRow>
+                         {employees.map((emp, index) => (
+                              <EmployeeRow key={index} employee={emp} />
+                         ))}
                     </TableBody>
                </Table>
           </TableContainer>
