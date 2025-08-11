6fcee8ec1eb5e11e00e479fb6bd30ce55d9a7351
Leonardo Bernardo <leonardo.bernardo2658@gmail.com>
2025-08-11 19:31:08 -0300

    fix: corrgindo schema de validação

--- src/validations/employeeSchema.ts
+++ src/validations/employeeSchema.ts
@@ -4,13 +4,13 @@ import {z} from 'zod';
 export const employeeSchema = z.object({
      titulo: z
           .string()
+          .trim()
           .min(2, 'O título deve ter no mínimo 2 caracteres')
           .max(50, 'O título deve ter no máximo 50 caracteres'),
      email: z
-          .string()
           .email('Email inválido')
           .max(100, 'O email deve ter no máximo 100 caracteres'),
-     departamento: z.string().min(1, 'Departamento é obrigatório'),
+     departamento: z.string().trim().nonempty('Departamento é obrigatório'),
      status: z.boolean(), // booleano para ativado/desativado
 });
 
