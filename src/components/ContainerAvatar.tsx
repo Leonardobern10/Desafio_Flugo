9bb3b1c1dfd703a5f680516e6c88550968ddd142
Leonardo Bernardo <leonardo.bernardo2658@gmail.com>
2025-08-11 19:44:00 -0300

    remove: removendo comentários desnecessários

--- src/components/ContainerAvatar.tsx
+++ src/components/ContainerAvatar.tsx
@@ -2,10 +2,6 @@ import {Box} from '@mui/material';
 import type {ReactElement} from 'react';
 import UserAvatar from './UserAvatar';
 
-/**
- * Talvez trocar esse box por um icone
- */
-
 export default function ContainerAvatar(): ReactElement {
      return (
           <Box sx={{justifySelf: 'flex-start'}}>
