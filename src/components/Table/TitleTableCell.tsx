cdf8dfafabd614feaf1bba347d02528d9e68d21f
Leonardo Bernardo <leonardo.bernardo2658@gmail.com>
2025-08-13 12:17:08 -0300

    feat: alteração de avatar por parte do usuario

--- src/components/Table/TitleTableCell.tsx
+++ src/components/Table/TitleTableCell.tsx
@@ -1,6 +1,6 @@
-import {Avatar, Box, TableCell, Typography} from '@mui/material';
+import {Avatar, Box, ButtonBase, TableCell, Typography} from '@mui/material';
 import type {TableCellType} from 'types/TableCellType';
-import type {ReactElement} from 'react';
+import {useState, type ReactElement} from 'react';
 
 /**
  * Componente TitleTableCell que renderiza uma célula de
@@ -16,6 +16,18 @@ export default function TitleTableCell({
      text,
      avatar,
 }: TableCellType): ReactElement {
+     const [currentAvatar, setCurrentAvatar] = useState<string>(avatar!);
+
+     const handleFileChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+          const file = e.target.files?.[0];
+          if (!file) return;
+
+          const reader = new FileReader();
+          reader.onload = () => {
+               setCurrentAvatar(reader.result as string); // atualiza o avatar temporário
+          };
+          reader.readAsDataURL(file);
+     };
      return (
           <TableCell align="left">
                <Box
@@ -26,7 +38,15 @@ export default function TitleTableCell({
                          alignItems: 'center',
                          columnGap: 2,
                     }}>
-                    <Avatar src={avatar} />
+                    <ButtonBase component="label" sx={{borderRadius: '50%'}}>
+                         <Avatar src={currentAvatar} />
+                         <input
+                              type="file"
+                              accept="image/*"
+                              style={{display: 'none'}}
+                              onChange={handleFileChange}
+                         />
+                    </ButtonBase>
                     <Typography fontSize="small">{text}</Typography>
                </Box>
           </TableCell>
